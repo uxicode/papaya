0d1105bef4d4fc0dc76c88d4007e277a587ee21e
Seok Jin Kim <sjkim@inition.kr>
2021-03-04 12:27:40 +0900

    modify myclass

--- src/views/home/myClass/MyClass.ts
+++ src/views/home/myClass/MyClass.ts
@@ -1,6 +1,6 @@
 import {Vue, Component, Prop} from 'vue-property-decorator';
 import ClassService from '@/api/service/MyClassService';
-import {IClassInfo, IMe, IMyClassList, IOwner} from '@/views/model/my-class.model';
+import {IClassInfo, IMyClassList} from '@/views/model/my-class.model';
 // @ts-ignore
 import WithRender from './MyClass.html';
 
@@ -12,9 +12,9 @@ export default class MyClass extends Vue {
             classThumb: require('@/assets/images/bg-icon.png'),
             className: '',
             classOwner: '',
-            createdYear: 2019,
+            createdYear: 0,
             classType: '',
-            memberNum: 2122,
+            memberNum: 0,
             isUpdated: true,
             updatedDiffTime: '',
             isBookmarked: 1,
@@ -96,7 +96,14 @@ export default class MyClass extends Vue {
             .then(() => {
                 //Promise.all 로 처리하면 리턴값이 배열. 즉 별도 매칭이 필요.
                 this.classItems.map( ( item: any, idx: number ) => {
-                    this.myClass[idx].className=item.myclass_list[idx].name;
+                    //this.myClass[idx].classThumb = item.myclass_list[idx].profile_image;
+                    this.myClass[idx].className = item.myclass_list[idx].name;
+                    this.myClass[idx].isBookmarked = item.myclass_list[idx].me.is_bookmarked;
+                    this.myClass[idx].classOwner = item.myclass_list[idx].owner.nickname;
+                    this.myClass[idx].createdYear=
+                        item.myclass_list[idx].owner.createdAt.substr(0,4); // 앞 4자리가 연도
+                    this.myClass[idx].classType=
+                        item.myclass_list[idx].owner.status === 1 ? '비공개클래스' : '공개클래스';
                 });
             });
     }
