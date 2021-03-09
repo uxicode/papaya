4d65e723776c2ef7b8cc470e1fd96b4e2b614e3f
Seok Jin Kim <sjkim@inition.kr>
2021-03-09 15:06:05 +0900

    mypage page inject

--- src/views/mypage/bookmark/Bookmark.ts
+++ src/views/mypage/bookmark/Bookmark.ts
@@ -1,9 +1,21 @@
 import {Vue, Component, Prop} from 'vue-property-decorator';
 import WithRender from './Bookmark.html';
+import Btn from '@/components/button/Btn.vue';
+import Modal from '@/components/modal/modal.vue';
 
 @WithRender
-@Component
+@Component({
+    components:{
+        Btn,
+        Modal,
+    },
+})
 
 export default class Bookmark extends Vue {
+    private isResetModal: boolean = false;
+    private bookmarkType: string = 'notification';
 
+    private resetModalOpen(): void {
+        this.isResetModal = !this.isResetModal;
+    }
 }
