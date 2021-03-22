6fd15a3d3df7923b3cec5b661909401c9bda5a35
bongchuljun <jbc@inition.kr>
2021-03-22 16:21:50 +0900

    modify router

--- src/router/MyClassRouter.ts
+++ src/router/MyClassRouter.ts
@@ -2,6 +2,8 @@ import AppHeader from '@/components/header/header.vue';
 import AppFooter from '@/components/footer/footer.vue';
 import {getIsAuth} from '@/router/AuthGuard';
 
+// @ts-ignore
+// @ts-ignore
 const MyClassRouter=[
   {
     path: '/',
@@ -15,25 +17,12 @@ const MyClassRouter=[
     components: {default: () => import('../views/class/home/MyClass'), header: AppHeader, footer: AppFooter},
     beforeEnter: getIsAuth,
   },
- /* {
-    path: '/class/notify',
-    name: 'notify',
-    components: {default: () => import('../views/class/notify/Notify'), header: AppHeader, footer: AppFooter},
-    beforeEnter: getIsAuth,
-  },
-  {
-    path: '/class/schedule',
-    name: 'schedule',
-    components: {default: () => import('../views/class/schedule/Schedule'), header: AppHeader, footer: AppFooter},
-    beforeEnter: getIsAuth,
-  },*/
-  //ex) - makeClass.4.3.1
   {
     path: '/make-class',
     name: 'makeClass',
-    components: {default: () => import('../views/class/makeClass/MakeClassPage'), header: AppHeader, footer: AppFooter},
+    components: {default: () => import('../views/class/make/MakeClassPage'), header: AppHeader, footer: AppFooter},
     children: [
-      {path: 'step1', name: 'makeClassOption', component: () => import('../views/class/makeClass/step1/MakeClassOption')}
+      {path: 'step1', name: 'makeClassOption', component: () => import('../views/class/make/step1/MakeClassOption')}
       // {path: 'step2', name: 'makeClassOption', component: () => import('../views/class/makeClass/step1/MakeClassOption')},
       // {path: 'step3', name: 'makeClassOption', component: () => import('../views/class/makeClass/step1/MakeClassOption')},
     ]
