0fedaa4a0a81ea1b9a1d1a13edebe357c69e07b0
Leeyejin <inition@yejin.local>
2021-04-09 09:36:46 +0900

    save

--- src/store/action-class-types.ts
+++ src/store/action-class-types.ts
@@ -3,9 +3,4 @@ export const MODIFY_MYCLASS_LIST_ACTION: string = 'MODIFY_MYCLASS_LIST_ACTION';
 export const POST_LIST_ACTION: string = 'POST_LIST_ACTION';
 export const MAKE_CLASS: string = 'MAKE_CLASS';
 export const MYCLASS_HOME: string = 'MYCLASS_HOME';
-<<<<<<< HEAD
-export const CLASS_MEMBER_INFO_ACTION: string = 'CLASS_MEMBER_INFO_ACTION';
-export const MODIFY_CLASS_MEMBER_INFO: string = 'MODIFY_CLASS_MEMBER_INFO';
-=======
 export const UPDATE_SIDE_MENU_NUM_ACTION: string = 'UPDATE_SIDE_MENU_NUM_ACTION';
->>>>>>> 97d9e83904744d00c1f8d9412798797c19179f51
