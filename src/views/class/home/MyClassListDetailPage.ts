1041980f66252629a3b5efefc4bd004dfae9fa9e
Leeyejin <inition@yejin.local>
2021-04-12 10:10:00 +0900

    save

--- src/views/class/home/MyClassListDetailPage.ts
+++ src/views/class/home/MyClassListDetailPage.ts
@@ -6,7 +6,6 @@ import Modal from '@/components/modal/modal.vue';
 import Btn from '@/components/button/Btn.vue';
 import MyClassService from '@/api/service/MyClassService';
 import WithRender from './MyClassListDetailPage.html';
-import {UPDATE_SIDE_MENU_NUM_ACTION} from '@/store/action-class-types';
 
 const MyClass = namespace('MyClass');
 
@@ -22,10 +21,6 @@ export default class MyClassListDetailPage extends Vue {
 
   private activeMenuNum: number=0;
 
-
-  @MyClass.Action
-  private UPDATE_SIDE_MENU_NUM_ACTION!: (num: number) => void;
-
   get activeMenuNumModel(): number {
     return this.activeMenuNum;
   }
@@ -33,20 +28,10 @@ export default class MyClassListDetailPage extends Vue {
   @MyClass.Getter
   private classID!: string | number;
 
-  public created(){
-    //화면 새로고침시에
-    if (performance.navigation.type === 1) {
-      this.activeMenuNum=0;
-      this.UPDATE_SIDE_MENU_NUM_ACTION(this.activeMenuNum);
-    }
-  }
-
 
   private update(idx: number): void{
+    // console.log(idx);
     this.activeMenuNum=idx;
-    console.log(this.activeMenuNum);
-    this.UPDATE_SIDE_MENU_NUM_ACTION(this.activeMenuNum);
-    // this.UPDATE_SIDE_MENU_NUM(idx);
   }
 
 }
