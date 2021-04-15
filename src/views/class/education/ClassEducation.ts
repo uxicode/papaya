8ad63385f81c81e7f276b77c3f8599c33c27e2b0
Leeyejin <inition@yejin.local>
2021-04-15 13:58:37 +0900

    save

--- src/views/class/education/ClassEducation.ts
+++ src/views/class/education/ClassEducation.ts
@@ -367,6 +367,7 @@ export default class ClassEducation extends Vue {
 
     private eduItems: Array< {title: string }>=[];
 
+
     get currListNumModel(): Array< {title: string }>{
         return this.eduItems;
     }
@@ -415,95 +416,89 @@ export default class ClassEducation extends Vue {
 
 
 
-
-
-
-
-
-
-
-
-    private isPopup: boolean=false;
-    private isTimeSelect: boolean=false;
-
-    private type: string= 'month';
-    private types: string[] = ['month', 'week', 'day', '4day'];
-    private mode: string= 'stack';
-    private modes: string[] = ['stack', 'column'];
-    private weekday: number[] = [1, 2, 3, 4, 5, 6, 0];
-    private weekdays: Array<{text: string, value: number[] }>=[
-        { text: 'Sun - Sat', value: [0, 1, 2, 3, 4, 5, 6] },
-        { text: 'Mon - Sun', value: [1, 2, 3, 4, 5, 6, 0] },
-        { text: 'Mon - Fri', value: [1, 2, 3, 4, 5] },
-        { text: 'Mon, Wed, Fri', value: [1, 3, 5] },
-    ];
-    private value: string= '';
-    private events: any[] = [];
-    private colors: string[] = ['#3F51B5', '#00BCD4', '#673AB7', '#2196F3', '#4CAF50', '#FF9800', '#757575'];
-    private names: string[] =  ['Meeting', 'Holiday', 'PTO', 'Travel', 'Event', 'Birthday', 'Conference', 'Party'];
-
-    //datepicker
-    private datePickerModel: string= new Date().toISOString().substr(0, 10);
-    private menu1: boolean= false;
-
-    @MyClass.Getter
-    // private myClassHomeModel!: IClassInfo;
-
-    // public getEvents( time: { start: any, end: any } ) {
-    //     const eventItems= [];
     //
-    //     const min = new Date(`${time.start.date}T00:00:00`);
-    //     const max = new Date(`${time.end.date}T23:59:59`);
-    //     const days = (max.getTime() - min.getTime()) / 86400000;
-    //     const eventCount = this.rnd(days, days + 20);
     //
-    //     for (let i = 0; i < eventCount; i++) {
-    //         const allDay = this.rnd(0, 3) === 0;
-    //         const firstTimestamp = this.rnd(min.getTime(), max.getTime());
-    //         const first = new Date(firstTimestamp - (firstTimestamp % 900000));
-    //         const secondTimestamp = this.rnd(2, allDay ? 288 : 8) * 900000;
-    //         const second = new Date(first.getTime() + secondTimestamp);
+    // private isPopup: boolean=false;
+    // private isTimeSelect: boolean=false;
     //
-    //         eventItems.push({
-    //             name: this.names[this.rnd(0, this.names.length - 1)],
-    //             start: first,
-    //             end: second,
-    //             color: this.colors[this.rnd(0, this.colors.length - 1)],
-    //             timed: !allDay,
-    //         });
-    //     }
+    // private type: string= 'month';
+    // private types: string[] = ['month', 'week', 'day', '4day'];
+    // private mode: string= 'stack';
+    // private modes: string[] = ['stack', 'column'];
+    // private weekday: number[] = [1, 2, 3, 4, 5, 6, 0];
+    // private weekdays: Array<{text: string, value: number[] }>=[
+    //     { text: 'Sun - Sat', value: [0, 1, 2, 3, 4, 5, 6] },
+    //     { text: 'Mon - Sun', value: [1, 2, 3, 4, 5, 6, 0] },
+    //     { text: 'Mon - Fri', value: [1, 2, 3, 4, 5] },
+    //     { text: 'Mon, Wed, Fri', value: [1, 3, 5] },
+    // ];
+    // private value: string= '';
+    // private events: any[] = [];
+    // private colors: string[] = ['#3F51B5', '#00BCD4', '#673AB7', '#2196F3', '#4CAF50', '#FF9800', '#757575'];
+    // private names: string[] =  ['Meeting', 'Holiday', 'PTO', 'Travel', 'Event', 'Birthday', 'Conference', 'Party'];
     //
-    //     this.events = eventItems;
+    // //datepicker
+    // private datePickerModel: string= new Date().toISOString().substr(0, 10);
+    // private menu1: boolean= false;
+    //
+    // @MyClass.Getter
+    // // private myClassHomeModel!: IClassInfo;
+    //
+    // // public getEvents( time: { start: any, end: any } ) {
+    // //     const eventItems= [];
+    // //
+    // //     const min = new Date(`${time.start.date}T00:00:00`);
+    // //     const max = new Date(`${time.end.date}T23:59:59`);
+    // //     const days = (max.getTime() - min.getTime()) / 86400000;
+    // //     const eventCount = this.rnd(days, days + 20);
+    // //
+    // //     for (let i = 0; i < eventCount; i++) {
+    // //         const allDay = this.rnd(0, 3) === 0;
+    // //         const firstTimestamp = this.rnd(min.getTime(), max.getTime());
+    // //         const first = new Date(firstTimestamp - (firstTimestamp % 900000));
+    // //         const secondTimestamp = this.rnd(2, allDay ? 288 : 8) * 900000;
+    // //         const second = new Date(first.getTime() + secondTimestamp);
+    // //
+    // //         eventItems.push({
+    // //             name: this.names[this.rnd(0, this.names.length - 1)],
+    // //             start: first,
+    // //             end: second,
+    // //             color: this.colors[this.rnd(0, this.colors.length - 1)],
+    // //             timed: !allDay,
+    // //         });
+    // //     }
+    // //
+    // //     this.events = eventItems;
+    // // }
+    //
+    // public created(){
+    //     console.log(new Date().toISOString());
     // }
-
-    public created(){
-        console.log(new Date().toISOString());
-    }
-
-    // public getDay(date: any){
-    //     const daysOfWeek = ['일', '월', '화', '수', '목', '금', '토'];
-    //     // console.log(date.weekday);
-    //     return daysOfWeek[date.weekday];
+    //
+    // // public getDay(date: any){
+    // //     const daysOfWeek = ['일', '월', '화', '수', '목', '금', '토'];
+    // //     // console.log(date.weekday);
+    // //     return daysOfWeek[date.weekday];
+    // // }
+    // // public getEventColor(event: any) {
+    // //     return event.color;
+    // // }
+    // // public rnd(a: any, b: any): number {
+    // //     return Math.floor((b - a + 1) * Math.random()) + a;
+    // // }
+    //
+    // private updatePopup(isOpen: boolean) {
+    //     this.isPopup=isOpen;
     // }
-    // public getEventColor(event: any) {
-    //     return event.color;
+    //
+    // private closePopup(): void{
+    //     this.isPopup=false;
     // }
-    // public rnd(a: any, b: any): number {
-    //     return Math.floor((b - a + 1) * Math.random()) + a;
+    //
+    // private addScheduleOpen(): void{
+    //     this.updatePopup(true);
     // }
-
-    private updatePopup(isOpen: boolean) {
-        this.isPopup=isOpen;
-    }
-
-    private closePopup(): void{
-        this.isPopup=false;
-    }
-
-    private addScheduleOpen(): void{
-        this.updatePopup(true);
-    }
-
+    //
 
 
 
