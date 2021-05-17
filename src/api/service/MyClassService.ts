cee861752df577fa84b6714987d07310fe1aa87c
조영은 <janecho0601@gmail.com>
2021-05-17 12:19:57 +0900

    save

--- src/api/service/MyClassService.ts
+++ src/api/service/MyClassService.ts
@@ -260,7 +260,7 @@ class MyClassService {
      * @param classId
      * @param curriculumId
      */
-    public getEduCurrList(classId: number, curriculumId: number): Promise<any>{
+    public getEduCurList(classId: number, curriculumId: number): Promise<any>{
         return request('get', `${CLASS_BASE_URL}/${classId}/curriculum/${curriculumId}` );
     }
 
